80ebde0fa5b5b9af9b6b08733a1b6c122c1c5ec4
stephenvarady <imnotstephenvarady@gmail.com>
2014-10-08 13:02:36 -0400

    updated index

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -2,7 +2,7 @@ class Shop < ActiveRecord::Base
 	has_many :roasters
 
 	geocoded_by :address
-	after_validation :geocode
+	# after_validation :geocode
 
 	def address
 		[street, city, state, country].compact.join(', ')
