55bb61f2a9ff9e0028bab9c39f27a10d908dad87
stephenvarady <imnotstephenvarady@gmail.com>
2014-10-09 15:32:02 -0400

    working on config bug

--- config/application.rb
+++ config/application.rb
@@ -6,8 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.serve_static_assets = true
-
 module Beanie
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -21,5 +19,6 @@ module Beanie
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.serve_static_assets = true
   end
 end
