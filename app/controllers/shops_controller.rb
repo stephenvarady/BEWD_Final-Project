51c0ffc1a5a816adea9142f0667d65c4343720bd
stephenvarady <imnotstephenvarady@gmail.com>
2014-10-09 13:34:17 -0400

    working shops map

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -5,9 +5,9 @@ class ShopsController < ApplicationController
 		@shops = Shop.all
 		@user = current_user
 		@location = request.location
-		@hash = Gmaps4rails.build_markers(@location) do |location, marker|
-		  marker.lat location.latitude
-		  marker.lng location.longitude
+		@hash = Gmaps4rails.build_markers(@shops) do |shop, marker|
+		  marker.lat shop.latitude
+		  marker.lng shop.longitude
 		end
 	end
 
